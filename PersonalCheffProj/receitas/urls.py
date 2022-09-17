fb73fb4a97311a2890fb77801282264381dab22e
Guto Xavier <gutoffline@gmail.com>
2022-09-17 13:47:02 -0300

    arquivo contato e menu

--- PersonalCheffProj/receitas/urls.py
+++ PersonalCheffProj/receitas/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contato', views.contato, name='contato'),
     path('sucodelaranja', views.sucodelaranja, name='sucodelaranja'),
     path('sucodelimao', views.sucodelimao, name='sucodelimao'),
 ]
--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
+def contato(request):
+    return render(request,'contato.html')
+
 def sucodelaranja(request):
     return render(request,'sucodelaranja.html')
 
